ae13906401282e825ef318a1407bfab38e2f01c8
git-osaku <osaku48@gmail.com>
2015-05-28 23:17:03 +0900

    json追加

--- LookAtThis/LookAtThis/ViewController.swift
+++ LookAtThis/LookAtThis/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -21,6 +22,12 @@ class ViewController: UIViewController {
             
         }
     }
+    
+    //array of entries
+    var entries = NSArray()
+    let newsUrlString = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://rss.itmedia.co.jp/rss/2.0/news_bursts.xml&num=8"
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +47,15 @@ class ViewController: UIViewController {
         let downGestrue = UISwipeGestureRecognizer(target: self, action: "downSwipe:")
         downGestrue.direction = UISwipeGestureRecognizerDirection.Down
         
+        let tapGestrue = UITapGestureRecognizer(target: self, action: "tap:")
+        
         imageView.addGestureRecognizer(rightGestrue)
         imageView.addGestureRecognizer(leftGestrue)
         imageView.addGestureRecognizer(upGestrue)
         imageView.addGestureRecognizer(downGestrue)
+        imageView.addGestureRecognizer(tapGestrue)
+        
+        self.getData()
     
     }
 
@@ -55,9 +67,6 @@ class ViewController: UIViewController {
     func rightSwipe(gestureRecognizer: UIGestureRecognizer){
         
         self.randomDog(0)
-        
-        
-        
     }
     
     func leftSwipe(gestureRecognizer: UIGestureRecognizer){
@@ -81,6 +90,12 @@ class ViewController: UIViewController {
         
     }
     
+    func tap(gestureRecognizer: UIGestureRecognizer){
+        
+         imageView.image = UIImage(named: "前.jpg")
+    }
+    
+    
     func randomDog(direction :Int) {
         
         let randInt = arc4random_uniform(4);
@@ -102,11 +117,10 @@ class ViewController: UIViewController {
         } else if randInt == 3 {
             
             imageView.image = UIImage(named: "下.jpg")
-            
+
         }
         
         if Int(randInt) == direction {
-            
             
             score++
             
@@ -116,9 +130,41 @@ class ViewController: UIViewController {
             score = 0
         }
         
+    }
+    
+    func getData() {
+        
+        // create the url-request
+        let urlString = "http://192.168.33.10:8000/ranking"
+        var request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+        
+        // set the method(HTTP-POST)
+        request.HTTPMethod = "POST"
+        // set the header(s)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // set the request-body(JSON)
+        var params: [String: AnyObject] = [
+            "foo": "bar",
+            "baz": [
+                "a": 1,
+                "b": 20,
+                "c": 300
+            ]
+        ]
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: nil)
+        
+        // use NSURLSessionDataTask
+        var task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {data, response, error in
+            if (error == nil) {
+                var result = NSString(data: data, encoding: NSUTF8StringEncoding)!
+
+                println(result)
+            } else {
+                println(error)
+                            }
+        })
+        task.resume()
         
     }
-
-
 }
-
